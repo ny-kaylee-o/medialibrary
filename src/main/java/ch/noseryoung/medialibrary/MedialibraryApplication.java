55d01e45371873d7041e109dc283c741946df3a6
ny-kaylee-o <kaylee.osagie@noseryoung.ch>
2026-04-20 08:37:45 +0200

    initial commit

---
+++ src/main/java/ch/noseryoung/medialibrary/MedialibraryApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.medialibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedialibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedialibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/noseryoung/medialibrary/MedialibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.medialibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedialibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
